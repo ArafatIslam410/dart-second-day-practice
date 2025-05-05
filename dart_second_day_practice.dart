0f79b3e51ad9c17022841c5467d6f52521b7d6b4
Md.Arafat Islam Nazmul <rahmannazmul10@gmail.com>
2025-05-05 11:13:53 +0600

    Add files via upload

---
+++ dart_second_day_practice.dart
@@ -0,0 +1,82 @@
+void main (){
+ /* int a = 10;
+  int b = 13;
+  int c = 18;
+  print(a~/b);
+  print (a%b);*/
+  /*dynamic x=4;
+  x+=3;
+  //print(x);
+  x-=2;
+  //print(x);
+  x*=3;
+  //print(x);
+  x/=3;
+  //print(x);
+ x~/=2;
+ print (x);*/
+  /*int a=35;
+  int b=45;
+  print(" is a bigger than b?: ${a>b}");
+  print("a is even?: ${a%2==0}");
+  print("b is odd?: ${b%2!=0}");*/
+  /*int num = 16;
+  if(num%2==0) {
+    print("number is even");
+  }else{
+    print("Number is odd");
+  }*/
+  /*int a = 23;
+  int b = 56;
+  int c = 67;
+  if(a>=b&&a>=c){
+    print("a lis largest");
+  }else if(b>=a&&b>=c){
+    print("b is largest");
+  }else {
+    print("c is largest");
+  }*/
+  /*int Marks = 56;
+  if(Marks>=80){
+    print("result is A+");
+  }else if(Marks>=70){
+    print("result is A");
+  }else if (Marks>=50){
+    print("tene tune pass ");
+  }else if(Marks>=40){
+    print("milad por allah bachaiya diche");
+  }else{
+    print("jao baba tmi fail");
+  }*/
+  /*int num =4;
+  if(num>0){
+    print("positive");
+  }else if (num<0){
+    print("negitive");
+  }else{
+    print("zero");
+  }*/
+  //int year = 1976;
+  /*if(year%4==0&&year%100!=0||year%400==0){
+    print("this is leap year");
+  }else{
+    print("this is not leap year");
+  }*/
+  /*int a=10,b=20;
+  int diff=(a-b).abs();
+  print("difference= $diff");*/
+  /*int taka =67;
+  int coin10= taka~/10;
+  int coin5=(taka%10)~/5;
+  int coin1=taka%5;
+  print("10 takar coin: $coin10");
+  print("5 takar coin :$coin5");
+  print("1 takar coin :$coin1");*/
+
+
+
+
+
+
+
+}
